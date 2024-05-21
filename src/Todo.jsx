877fbd21e1333d752ecd5eb5a7bdefb6a00cc24a
dongkyusq <hasd789@naver.com>
2024-05-21 21:05:49 +0900

    todolist

---
+++ src/Todo.jsx
@@ -0,0 +1,33 @@
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="write todo"
+        />
+        <button>plus todo</button>
+      </form>
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
